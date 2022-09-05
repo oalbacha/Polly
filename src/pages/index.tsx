230df0354587b9345999561e5c3426e01c54bfac
Omar Albacha <oalbacha@gmail.com>
2022-09-05 20:03:12 +0400

    extend db question length

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,35 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import React from "react";
 import { prisma } from "../db/client";
 import { trpc } from "../utils/trpc";
 
-const Home: NextPage = (props: any) => {
+const QuestionCreator: React.FC = () => {
+  const { mutate } = trpc.useMutation("questions.create");
+
+  return (
+    <input
+      onSubmit={(e) => {
+        console.log(e.currentTarget.value);
+      }}
+    ></input>
+  );
+};
+
+const Home: NextPage = () => {
   const { isLoading, data } = trpc.useQuery(["questions.get-all"]);
   console.log("data:", data);
-  if (isLoading || !data) {
-    <div>Loading...</div>;
-  }
-  return <div>{data ? <div>{data[0]?.question}</div> : null}</div>;
+  if (isLoading || !data) return <div>Loading...</div>;
+
+  return (
+    <div>
+      <div>
+        <div className="text-2xl font-bold">Questions:</div>
+        {data[0]?.question}
+      </div>
+      <QuestionCreator />
+    </div>
+  );
 };
 
 export default Home;
