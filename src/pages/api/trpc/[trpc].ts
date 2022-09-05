ad538123d4aca4fd30638417f5532b399cf440cb
Omar Albacha <oalbacha@gmail.com>
2022-09-05 17:30:40 +0400

    add superjson

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -2,9 +2,11 @@ import * as trpc from "@trpc/server";
 import * as trpcNext from "@trpc/server/adapters/next";
 import { z } from "zod";
 import { prisma } from "../../../db/client";
+import superjson from "superjson";
 
 export const appRouter = trpc
   .router()
+  .transformer(superjson)
   .query("hello", {
     input: z
       .object({
