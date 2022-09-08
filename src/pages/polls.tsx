b29011aae29eb81d6cf61faafa80f2613185075f
Omar Albacha <oalbacha@gmail.com>
2022-09-08 13:57:51 +0400

    voter tokens

--- src/pages/polls.tsx
+++ src/pages/polls.tsx
@@ -4,6 +4,17 @@ import React from "react";
 import { trpc } from "../utils/trpc";
 
 const Polls: NextPage = () => {
+  function formatDate(date: Date) {
+    var d = new Date(date),
+      month = "" + (d.getMonth() + 1),
+      day = "" + d.getDate(),
+      year = d.getFullYear();
+
+    if (month.length < 2) month = "0" + month;
+    if (day.length < 2) day = "0" + day;
+
+    return [year, month, day].join("-");
+  }
   const { isLoading, data } = trpc.useQuery(["questions.get-all"]);
   console.log("data:", data);
   if (isLoading || !data) return <div>Loading...</div>;
@@ -13,8 +24,11 @@ const Polls: NextPage = () => {
       <div className="text-2xl font-bold">Questions:</div>
       {data.map((question) => (
         <Link href={`/question/${question.id}`} key={question.id}>
-          <a className="block font-bold uppercase tracking-wide text-sm">
-            {question.question}
+          <a className="block text-sm font-bold tracking-wide uppercase">
+            <p>{question.question}</p>
+            <p className="text-sm italic font-normal tracking-wide text-gray-500 capitalize">
+              Created: {formatDate(question?.createdAt)}
+            </p>
           </a>
         </Link>
       ))}
