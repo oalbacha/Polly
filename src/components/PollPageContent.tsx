3c1e5c9667f7c6eec3c849c105d1cbf1b0fb65e7
Omar Albacha <oalbacha@gmail.com>
2022-09-14 12:48:58 +0400

    add temp fix of flicker with poll content page on initial load. TODO: fix

--- src/components/PollPageContent.tsx
+++ src/components/PollPageContent.tsx
@@ -4,8 +4,12 @@ import PublishButton from "./PublishButton";
 import VoteButton from "./VoteButton";
 import PollOption from "./PollOption";
 import PollResults from "./PollResults";
+import useTimeout from "../hooks/useTimeout";
 
 const PollPageContent: React.FC<{ id: string }> = ({ id }) => {
+  const [visible, setVisible] = useState(true);
+  const hide = () => setVisible(false);
+  useTimeout(hide, 2200);
   const [viewPollResults, setViewPollResults] = useState(false);
   const { data, isLoading, isError, error } = trpc.useQuery([
     "polls.get-by-id",
@@ -18,68 +22,76 @@ const PollPageContent: React.FC<{ id: string }> = ({ id }) => {
 
   return (
     <div className="flex flex-col gap-3 p-8">
-      {data?.isOwner && (
-        <div className="w-32 text-center bg-indigo-100 text-indigo-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-indigo-200 dark:text-indigo-900">
-          You own this poll
-        </div>
-      )}
-      {data?.expired && (
-        <div className="w-28 text-center bg-red-100 text-red-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-red-200 dark:text-red-900">
-          Poll has ended
-        </div>
-      )}
-      {data?.isOwner && !data?.poll.isPublished && !data.expired && (
-        <PublishButton id={data.poll.id} />
-      )}
-      <div className="text-2xl font-bold">{data?.poll?.text}</div>
-      <div className="flex items-start justify-between w-[80%] gap-20 p-4">
-        <div className="flex gap-5 font-medium">
-          {(data?.poll?.options).map((item, idx) => {
-            const { vote, votes, isOwner, expired, poll, voteMetaData } = data;
-            if (vote || isOwner || expired) {
-              return (
-                <PollOption
-                  poll={poll as object}
-                  key={idx}
-                  optionId={vote?.optionId}
-                  itemId={item.id}
-                  itemText={item.text}
-                  voteCount={votes?.[idx]?._count}
-                />
-              );
-            } else {
-              return (
-                <VoteButton
-                  key={idx}
-                  itemText={item.text}
-                  pollId={poll?.id as string}
-                  optionId={item.id}
-                />
-              );
-            }
-          })}
-        </div>
+      {visible ? (
+        <div>Loading...</div>
+      ) : (
+        <div>
+          {data?.isOwner && (
+            <div className="w-32 text-center bg-indigo-100 text-indigo-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-indigo-200 dark:text-indigo-900">
+              You own this poll
+            </div>
+          )}
+          {data?.expired && (
+            <div className="w-28 text-center bg-red-100 text-red-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-red-200 dark:text-red-900">
+              Poll has ended
+            </div>
+          )}
+          {data?.isOwner && !data?.poll.isPublished && !data.expired && (
+            <PublishButton id={data.poll.id} />
+          )}
+          <div className="text-2xl font-bold">{data?.poll?.text}</div>
+          <div className="flex items-start justify-between w-[80%] gap-20 p-4">
+            <div className="flex gap-5 font-medium">
+              {(data?.poll?.options).map((item, idx) => {
+                const { vote, votes, isOwner, expired, poll, voteMetaData } =
+                  data;
 
-        {Array.isArray(data.votes) && data.votes.length > 0 && (
-          <div className="flex w-full flexx-col">
-            {!viewPollResults && (
-              <button
-                className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-7"
-                onClick={() => setViewPollResults(true)}
-              >
-                View Results
-              </button>
-            )}
-            {viewPollResults && (
-              <PollResults
-                voteMetaData={data.voteMetaData}
-                poll={data.poll}
-                votes={data.votes}
-              />
+                if (vote || isOwner || expired) {
+                  return (
+                    <PollOption
+                      poll={poll as object}
+                      key={idx}
+                      optionId={vote?.optionId}
+                      itemId={item.id}
+                      itemText={item.text}
+                      voteCount={votes?.[idx]?._count}
+                    />
+                  );
+                } else {
+                  return (
+                    <VoteButton
+                      key={idx}
+                      itemText={item.text}
+                      pollId={poll?.id as string}
+                      optionId={item.id}
+                    />
+                  );
+                }
+              })}
+            </div>
+
+            {Array.isArray(data.votes) && data.votes.length > 0 && (
+              <div className="flex w-full flexx-col">
+                {!viewPollResults && (
+                  <button
+                    className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-7"
+                    onClick={() => setViewPollResults(true)}
+                  >
+                    View Results
+                  </button>
+                )}
+                {viewPollResults && (
+                  <PollResults
+                    voteMetaData={data.voteMetaData}
+                    poll={data.poll}
+                    votes={data.votes}
+                  />
+                )}
+              </div>
             )}
           </div>
-        )}
-      </div>
+        </div>
+      )}
     </div>
   );
 };
